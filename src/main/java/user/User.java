14e17e4adfc82becc114927fcb946a5ac80600b7
Zhenyang Feng <zhenyangfeng@gmail.com>
2022-03-01 10:49:12 +0200

    Users added

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -1,4 +1,36 @@
 package user;
 
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ticket.Ticket;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Builder
+@Data
+@Table
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String name;
+    @NotNull
+    private String surname;
+    @NotNull
+    private String email;
+    @NotNull
+    private String password;
+//    @OneToMany
+//    List<Ticket> tickets;
+//
+    
+
 }
