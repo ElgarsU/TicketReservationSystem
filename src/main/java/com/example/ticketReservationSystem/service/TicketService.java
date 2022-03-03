eec1926e8f808fbf706f32438ae493a5aeb4ce1c
Mehmet Dikici <mehmetdi@gmail.com>
2022-03-03 09:57:27 +0200

    implement ticket model

--- src/main/java/com/example/ticketReservationSystem/service/TicketService.java
+++ src/main/java/com/example/ticketReservationSystem/service/TicketService.java
@@ -1,4 +1,16 @@
 package com.example.ticketReservationSystem.service;
 
+import com.example.ticketReservationSystem.model.Ticket;
+import com.example.ticketReservationSystem.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TicketService {
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    public Ticket getTicketById(Long id){
+        return ticketRepository.findById(id).orElse(null);
+    }
 }
